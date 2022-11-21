195810642c8c7e0039e956de433065aeb3b8aa99
leocherubini <cherubini18@gmail.com>
2022-11-21 15:19:07 -0300

    Correcao redirecionamento

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,8 @@
-$('button').click(function() {
-   window.location.href = window.location.href + "/table.html";
+$('button').click(function(e) {
+   //window.location.href = env + "table.html";
+//window.location.href = "google.com";
+//window.location.href = "http://www.devmedia.com.br";
 //console.log('teste');
+e.preventDefault();
+window.location.href = env + "table.html";
 });
